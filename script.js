e0165bc4743ad08b58ecd9ea031626ee9abc4877
LarissaBSouza <larissabandeirasouza@gmail.com>
2022-06-22 21:14:39 -0300

    Oitavo Commit

--- script.js
+++ script.js
@@ -137,6 +137,55 @@ function pauseTrack() {
 
 function nextTrack() {
     if (track_index < music_list.length - 1 && isRandom === false) {
-        
+        track_index += 1;
+    } else if (track_index < music_list.length -1 && isRandom === true) {
+        let random_index = Number.parseInt(Math.random() * music_list.length);
+        track_index = random_index;
+    } else {
+        track_index = 0;
+    }
+
+    loadTrack(track_index);
+    playTrack();
+}
+
+function prevTrack() {
+    if (track_index > 0) {
+        track_index -= 1;
+    } else {
+        track_index = music_list.length -1;
+    }
+
+    loadTrack(track_index);
+    playTrack();
+}
+
+function seekTo() {
+    let seekto = curr_track.duration * (seek_slider.value / 100);
+    curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+    curr_track.volume = volume_slider.value /  100;
+}
+
+function setUpdate() {
+    let seekPosition = 0;
+    if (!isNaN(curr_track.duration)) {
+        seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+        seek_slider.value = seekPosition;
+
+        let currentMinutes = Math.floor(curr_track.currentTime / 60);
+        let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(curr_track.duration / 60);
+        let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+        if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+        if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+        if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+        if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+        curr_time.textContent = currentMinutes + ":" + currentSeconds;
+        total_duration.textContent = durationMinutes + ":" + durationSeconds;
     }
 }
